d4ea2cd971ea4a43be05905999559c15e3cd5ce7
Ch4zm <jaytripathi100@gmail.com>
2025-04-09 23:33:01 -0700

    done with basic assignment

--- asgn1/asgn1.js
+++ asgn1/asgn1.js
@@ -42,6 +42,8 @@ function main() {
     
     // Draw a point
     gl.drawArrays(gl.POINTS, 0, 1);
+
+    // drawTriangle([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
 }
 
 function setupWebGL() {
@@ -53,7 +55,8 @@ function setupWebGL() {
     }
     
     // Get the rendering context for WebGL
-    gl = getWebGLContext(canvas);
+    // gl = getWebGLContext(canvas);
+    gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
     if (!gl) {
         console.log('Failed to get the rendering context for WebGL');
         return;
@@ -66,6 +69,7 @@ function connectVariablesToGLSL() {
         console.log('Failed to intialize shaders.');
         return;
     }
+    
     // Get the storage location of a_Position
     a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     if (a_Position < 0) {
@@ -79,12 +83,12 @@ function connectVariablesToGLSL() {
         console.log('Failed to get the storage location of u_FragColor');
         return;
     }
-
+    
     // Get the storage location of u_PointSize
     u_PointSize = gl.getUniformLocation(gl.program, 'u_PointSize');
     if (!u_PointSize) {
         console.log('Failed to get the storage location of u_PointSize');
-        return;
+        return; 
     }
 }
 
@@ -96,7 +100,7 @@ function getRenderingContext(id) {
 function clearCanvas() {
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
+    
     g_shapes = [];
     renderAllShapes()
 }
@@ -104,55 +108,115 @@ function clearCanvas() {
 let g_shapes = [];  // The array for shapes, including color, size, and position
 
 function click(event) {
-    
+    console.log('current shape: ' + selectedShape);
     [x, y] = convertCoordinatesEventToGL(event);
     // Store the coordinates to points array
-    const point = new Point();
-    point.position = [x, y, 0.0];
-    point.color = g_selectedColor.slice();
-    point.size = Number(g_selectedSize);
-    g_shapes.push(point);
+    let shape = null;
+    switch(selectedShape) {
+        case 'point':
+            shape = new Point();
+            break;
+        case 'triangle':
+            shape = new Triangle();
+            break;
+        case 'circle':
+            shape = new Circle();
+            shape.segments = g_selectedSegments;
+            break;
+        default:
+            console.log('Unknown shape type');
+            return;
+    }
+    // shape = new Point();
 
+    shape.position = [x, y, 0.0];
+    shape.color = g_selectedColor.slice();
+    shape.size = Number(g_selectedSize);
+    g_shapes.push(shape);
+    
     renderAllShapes();
 }
 
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0]; // The current color of the selected point
 let g_selectedSize = 10.0; // The current size of the selected point
+let g_selectedSegments = 10; // The current number of segments for the selected circle
 
+let selectedShape = 'point'; // The current selected shape  
 function setupUICallbacks() {
-
     canvas.onmousedown = click;
-
+    canvas.onmousemove = function(event) {
+        if (event.buttons == 1) {
+            click(event);
+        }
+    }
     document.getElementById('clear').addEventListener('click', function() {
         clearCanvas();
     });
-
-    document.getElementById('redSlider').addEventListener('mouseup', function() {
+    
+    document.getElementById('redSlider').addEventListener('mousemove', function() {
         g_selectedColor[0] = Number(this.value) / 255.0;
     });
-    document.getElementById('greenSlider').addEventListener('mouseup', function() {
+    document.getElementById('greenSlider').addEventListener('mousemove', function() {
         g_selectedColor[1] = Number(this.value) / 255.0;
     });
-    document.getElementById('blueSlider').addEventListener('mouseup', function() {
+    document.getElementById('blueSlider').addEventListener('mousemove', function() {
         g_selectedColor[2] = Number(this.value) / 255.0;
     });
-
+    
     document.getElementById('sizeSlider').addEventListener('mousemove', function() {
         g_selectedSize = Number(this.value);
         document.getElementById('sizeLabel').innerText = this.value;
     });
 
+    document.getElementById('segmentsSlider').addEventListener('mousemove', function() {
+        g_selectedSegments = Number(this.value);
+        document.getElementById('segmentsLabel').innerText = this.value;
+    });
+
+    document.getElementById('squareButton').addEventListener('click', function() {
+        g_selectedColor = [1.0, 1.0, 1.0, 1.0]; // White
+        g_selectedSize = 10.0;
+        selectedShape = 'point';
+        updateSelectedColor();
+    });
+
+    document.getElementById('triangleButton').addEventListener('click', function() {
+        g_selectedColor = [1.0, 0.0, 0.0, 1.0]; // Red
+        g_selectedSize = 10.0;
+        selectedShape = 'triangle';
+        updateSelectedColor();
+    });
+
+    document.getElementById('circleButton').addEventListener('click', function() {
+        g_selectedColor = [0.0, 1.0, 0.0, 1.0]; // Green
+        g_selectedSize = 10.0;
+        selectedShape = 'circle';
+        updateSelectedColor();
+    });
+}
+
+function updateSelectedColor() {
+    const red = document.getElementById('redSlider').value;
+    const green = document.getElementById('greenSlider').value;
+    const blue = document.getElementById('blueSlider').value;
+    g_selectedColor = [red / 255.0, green / 255.0, blue / 255.0, 1.0];
 }
 
 function renderAllShapes() {
+    var startTime = performance.now();
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
-
+    
     var len = g_shapes.length;
-    console.log('len', len);
     for(var i = 0; i < len; i++) {
         g_shapes[i].render();
     }
+    
+    var duration = performance.now() - startTime;
+    document.getElementById('performanceLabel').innerText = 'numdot: ' +
+    len + ' ms: ' + Math.floor(duration) + ' fps: ' +
+    Math.floor(10000.0 /duration);
+    // console.log('numdot: ' + len + ' ms: ' + Math.floor(duration) + ' fps: ' + Math.floor(10000.0 /duration));
 }
 
 function convertCoordinatesEventToGL(event) {
